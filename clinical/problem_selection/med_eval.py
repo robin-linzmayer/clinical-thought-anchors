e5ff23f53fbc850b3e7c644a50d4660dc69f8985
Robin Linzmayer <ril2116@axon.dbmi.columbia.edu>
2026-02-18 10:24:50 -0500

    inference run on a few problems. need to fix evaluator.

--- clinical/problem_selection/med_eval.py
+++ clinical/problem_selection/med_eval.py
@@ -5,7 +5,7 @@ import sys
 from pathlib import Path
 
 # Add project root to path so we can import utils
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 from utils import extract_boxed_answers
 
 
